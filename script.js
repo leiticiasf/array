f9a56204a3b5269a3f9d82310f4b73b94b2b0df0
Letícia <111643803+leiticiasf@users.noreply.github.com>
2023-08-25 11:45:21 -0300

    a

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let listaNome = []; //varíavel utilizável em todas as funções (variável glo
 let listaProf = []
 
 function ver(){
-    console.log(listaNome + listaProf)
-}
+    console.log(listaNome)
+    console.log(listaProf)
 
 function adc(){
     let nome = document.getElementById("nome").value;
@@ -25,27 +25,24 @@ function rmv(){
     let nome = document.getElementById("nome").value;
     let profissao = document.getElementById("profissao").value;
     let pos = listaNome.indexOf(nome); //retorna o número referente a posição do produto requerido. 
-    if(pos == -1){  
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Algo deu errado! ' + ' A pessoa ' + nome + ' e a profissão ' + profissao + ' não foram encontrados!',
-            color: '#de6a04',
-            background: '#360101'
-            //para caso o produto pesquisado não exista, o valor retornado será de -1.
-          })
-          
-    
-    }
-    else{
-        listaNome.splice(pos, 1); //vai remover a quantidade de posições do array. 
-        listaProf.splice(pos, 1)
-        console.log("O nome " + nome + " e a profissão " + profissao + " foram removidos.");
+    if(pos !== -1){  
+    listaNome.splice(pos, 1); //vai remover a quantidade de posições do array. 
+    listaProf.splice(pos, 1)
         Swal.fire({
             icon: 'success',
             title: 'Sucesso!',
             text: 'O nome e a profissão foram removidas com êxito!',
             color: '#de6a04',
             background: '#360101'
-    })
+    }) 
+    }
+    else{
+        Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Algo deu errado! ' + ' A pessoa ' + nome + ' e a profissão ' + profissao + ' não foram encontrados!',
+        color: '#de6a04',
+        background: '#360101'
+        //para caso o produto pesquisado não exista, o valor retornado será de -1.
+      })    
 }}
